d15f233e5a791d27a816bab30c77543118c5e7f7
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-15 21:40:22 +0300

    minor tweaks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  attr_accessible :comment, :comment_date
+  attr_accessible :comment, :comment_date, :post_id, :user_id
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,4 @@
 class Like < ActiveRecord::Base
   belongs_to :post
+  attr_accessible :post_id, :user_id
 end
