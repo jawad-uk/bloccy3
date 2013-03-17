426c5b624143233a60c1d3177bafcce7b14f7600
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-17 21:52:08 +0300

    setting up routes & basic views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,11 +4,47 @@ class UserController < ApplicationController
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def new
+  	@user = User.new
   end
 
   def delete
   end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render :edit
+  	end
+  end
+
+  def update
+  	@user = User.new(params[:id])
+  	if @user.update_attributes(params[:user])
+  		redirect_to @user
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	if @user.destroy
+  			redirect_to users_path
+  	else
+  			flash[:error] = "Error deleting user"
+  			redirect_to @user
+    end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,5 @@
 Bloccy3::Application.routes.draw do
 
-  get "posts/index"
-
-  get "posts/edit"
-
-  get "posts/new"
-
-  get "posts/show"
-
-  get "user/index"
-
-  get "user/show"
-
-  get "user/new"
-
-  get "user/delete"
-
-  get "index/new"
-
-  get "index/edit"
-
-  get "index/show"
-
 resources :posts 
   
 resources :users do 
@@ -29,3 +7,5 @@ resources :users do
     resources :comments
   end
 end
+
+root :to => 'posts#index'
