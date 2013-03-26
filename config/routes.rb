7eefb894de0022666ba7e543ab8ea7f2f5e81d30
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-26 17:44:51 +0300

    updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bloccy3::Application.routes.draw do
 
 resources :posts 
-resources :user
+resources :users
 # resources :users do 
 #     resources :posts, only: [:index, :show]
 #     resources :comments
