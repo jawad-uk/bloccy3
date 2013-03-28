aaa3b04369ea462ad479101beeedabc890939aac
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-28 08:28:01 +0300

    post model update

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :likes
-  attr_accessible :body, :published_date, :tag, :title
+  attr_accessible :body, :published_date, :tag, :title, :comments_attributes
   accepts_nested_attributes_for :comments
 
 end
