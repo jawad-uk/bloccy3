680dece4ced554c0d68f00039eaa711fffca19ca
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-29 14:32:48 +0300

    removed redundant user scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :comments_from_authored_posts, :class_name => 'Comment', :through => :posts
   has_many :posts
   validates_presence_of :name, :email, :password
-  scope :recently_active, User.comments.order("created_at DESC")
+  # scope :recently_active, User.comments.order("created_at DESC")
   # after_create : send_email
   # before_save :encrypt_password
 end
