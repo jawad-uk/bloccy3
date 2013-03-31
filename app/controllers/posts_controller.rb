32364a3bdbe71a9bbf22975191ef255c24666dee
Jawad Abdulsamad <jawaad.abdulsamad@gmail.com>
2013-03-31 18:49:07 +0300

    fixed abilities model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
-  #before_filter :authenticate_user!, except: [:index, :show]
+  
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
   	@posts = Post.recent
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,19 @@
 class Ability
   include CanCan::Ability
-  
+
+  def initialize(user)
+    user ||= User.new #guest user
+
+    if user.author == true
+      can :manage, :all
+
+    else 
+        can :read, :all
+        can :create, :Comment
+        can :update, Comment do |comment|
+          comment.try(:user) == user
+            end
+    end
+  end
+
 end
